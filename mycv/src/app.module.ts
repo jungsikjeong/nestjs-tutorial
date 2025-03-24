6b7e91bceeb267a96415031df51caf903efa34fa
jungsikJeong <wndtlr1024@gmail.com>
2025-03-24 21:47:21 +0900

    튜또리얼쓰

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -4,14 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
-      synchronize: true,
+      entities: [User, Report],
+      synchronize: true, // 절대 운영환경에서는 안됌, 개발 환경 전용으로 TypeORM이 모든 엔터티 구조를 살펴본 후에 자동으로 데이터베이스 구조를 업데이트해줌, 테이블을 생성하고 삭제 및 열 추가 삭제, 이 열에 저장된 데이터 타입을 변경하기도함
     }),
     UsersModule,
     ReportsModule,
--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO(Data Transfer Object)에 정의되지 않은 속성을 자동으로 제거하는 기능을 활성화
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
