b5adea8dfa49b001e645740dfccd0851ea28bef5
JungSik Jeong <wndtlr1024@gmail.com>
2025-03-31 06:49:31 +0900

    temp: .

---
+++ mycv/src/users/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email in use');
+    }
+  }
+
+  async signin() {}
+}
