5b81c596ee63880e001d37a155098a38b2c0595d
JungSik Jeong <jungsik.jeong@lcnine.kr>
2025-04-01 20:37:05 +0900

    세션 설정하기

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['asdasd'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // DTO(Data Transfer Object)에 정의되지 않은 속성을 자동으로 제거하는 기능을 활성화
