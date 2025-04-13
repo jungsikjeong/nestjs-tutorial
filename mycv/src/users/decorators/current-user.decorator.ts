6766a4b290e89ca43d007513ae846b481ddf55c3
JungSik Jeong <wndtlr1024@gmail.com>
2025-04-14 06:57:19 +0900

    currentUser 인터셉터

--- mycv/src/users/decorators/current-user.decorator.ts
+++ mycv/src/users/decorators/current-user.decorator.ts
@@ -4,6 +4,6 @@ export const CurrentUser = createParamDecorator(
   (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     request.session.userId;
-    return 'hi there!';
+    return request.currentUser;
   },
 );
---
+++ mycv/src/users/interceptor/current-user.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { UsersService } from '../users.service';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private usersService: UsersService) {}
+
+  async intercept(context: ExecutionContext, handler: CallHandler) {
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session || {};
+
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
+      request.currentUser = user;
+    }
+
+    // 대상 라우트 핸들러를 실행하라는 의미
+    return handler.handle();
+  }
+}
