9e6dd5c5411d2203cf6873159a284b9da0534f02
JungSik Jeong <wndtlr1024@gmail.com>
2025-04-22 06:47:52 +0900

    전역적인 인터셉터 설정하기

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -8,24 +8,19 @@ import {
   Patch,
   Post,
   Query,
-  Session,
-  UseInterceptors,
+  Session
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
-import { UsersService } from './users.service';
-import { AuthService } from './auth.service';
-import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptor/current-user.interceptor';
 import { User } from './user.entity';
-
-
+import { UsersService } from './users.service';
 
 @Controller('auth')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
--- mycv/src/users/users.module.ts
+++ mycv/src/users/users.module.ts
@@ -5,10 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
 import { CurrentUserInterceptor } from './interceptor/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [UsersService, AuthService,
+    // 인터셉터 전역 설정
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor
+    }
+  ],
 })
 export class UsersModule { }
