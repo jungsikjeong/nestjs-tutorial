7d2ffd2c992b8a6532095e942c450491ab1700bf
jungsikjeong <wndtlr1024@gmail.com>
2025-04-29 06:47:37 +0900

    테스트 코드

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -2,10 +2,21 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 
-if ('can create an instance of auth service', async () => {
+it ('can create an instance of auth service', async () => {
+    // Create a fake copy of the users service
+    const fakeUsersService = {
+        find:()=>Promise.resolve([]), 
+        create:(email:string, password:string)=>Promise.resolve({id:1, email, password})
+    }
+
     const module = await Test.createTestingModule({
-        providers: [AuthService]
+        providers: [AuthService,{
+            provide:UsersService,
+            useValue:fakeUsersService
+        }]
     }).compile();
 
     const service = module.get(AuthService)
+
+    expect(service).toBeDefined();
 })
