1277f0949d18a68d28dbca36746620973778c626
jungsikjeong <wndtlr1024@gmail.com>
2025-05-02 07:21:12 +0900

    테스트

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -1,22 +1,32 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
-it ('can create an instance of auth service', async () => {
-    // Create a fake copy of the users service
-    const fakeUsersService = {
-        find:()=>Promise.resolve([]), 
-        create:(email:string, password:string)=>Promise.resolve({id:1, email, password})
-    }
+// 테스트시 'AuthService' 라는 이름으로 테스트 진행
+describe('AuthService', ()=>{
+    let service: AuthService;
 
-    const module = await Test.createTestingModule({
-        providers: [AuthService,{
-            provide:UsersService,
-            useValue:fakeUsersService
-        }]
-    }).compile();
-
-    const service = module.get(AuthService)
-
-    expect(service).toBeDefined();
+    beforeEach(async()=>{
+          // Create a fake copy of the users service
+          const fakeUsersService: Partial<UsersService> = {
+            find:()=>Promise.resolve([]), 
+            create:(email:string, password:string)=>Promise.resolve({id:1, email, password} as User)
+        }
+    
+        const module = await Test.createTestingModule({
+            providers: [AuthService,{
+                provide:UsersService,
+                useValue:fakeUsersService
+            }]
+        }).compile();
+    
+         service = module.get(AuthService)
+    
+    })
+    
+    it ('can create an instance of auth service', async () => {
+      
+        expect(service).toBeDefined();
+    })     
 })
